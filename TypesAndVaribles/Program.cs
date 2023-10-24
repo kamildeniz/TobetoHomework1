76b1789ab862b310d1d9b84869cb8c8d2f5eb3a8
kamil07deniz <kamil07deniz@gmail.com>
2023-10-25 00:08:47 +0300

    int32 data limit

--- TypesAndVaribles/Program.cs
+++ TypesAndVaribles/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Value Types
 Console.WriteLine("Hello, World!");
+// int veri tipi =  -2147483648 ile 2147483647 sınırlıdır.
 int number1 = 10;
 Console.WriteLine("Number is[0}",number1);
