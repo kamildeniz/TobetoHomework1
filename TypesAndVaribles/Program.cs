e3227947d5602a80dbe63c58c90160dae2f49a0f
kamil07deniz <kamil07deniz@gmail.com>
2023-10-25 01:42:14 +0300

    Data Types

--- TypesAndVaribles/Program.cs
+++ TypesAndVaribles/Program.cs
@@ -3,4 +3,51 @@
 Console.WriteLine("Hello, World!");
 // int veri tipi =  -2147483648 ile 2147483647 sınırlıdır.
 int number1 = 10;
-Console.WriteLine("Number is[0}",number1);
+Console.WriteLine("Number is {0}", number1);
+
+//long veri tipi = -9223372036854775808 ile 9223372036854775807 sınırlıdır
+long number2 = -9223372036854775808;
+Console.WriteLine("Number2 is {0}", number2);
+
+// short veri tipi = -32768 ile 32767 sınırlıdır
+short number3 = 32767;
+Console.WriteLine("Number3 is {0}", number3);
+
+
+// byte veri tipi => 0 ile 255 arasındadır
+byte number4 = 255;
+Console.WriteLine("Number4 is {0}", number4);
+// bool veri tipi = true/false.
+bool condition = false;
+Console.WriteLine("MyCondition is {0}", condition);
+
+// char tipi tek karakter verileri tutar
+char character = 'A';
+Console.WriteLine("Character is {0}", character);
+Console.WriteLine("Character is {0}", (int)character); // Unboxing
+
+// double veri tipi
+double number5 = 10.4;
+Console.WriteLine("Number5 is {0}", number5);
+
+// decimal veri tipi
+decimal number6 = 10.4m;
+Console.WriteLine("Number6 is {0}", number6 + " (decimal)");
+
+
+// enum sabitleri
+Console.WriteLine("enum sabiti => " + Days.Friday);
+Console.WriteLine("enum sabitinin değeri => " + (int)Days.Friday);
+
+// var keyword'ü
+var number7 = 10;
+number7 = 'A';
+//number7 = "A"  //yanlış kullanım!
+Console.WriteLine("Number7 is {0}", number7);
+
+Console.ReadKey();
+
+enum Days // enum tanımlaması
+{
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
